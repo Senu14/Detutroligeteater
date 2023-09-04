ca21719981185722db27b559c4d31414eff1215f
Senu14 <101559338+Senu14@users.noreply.github.com>
2023-09-04 14:38:34 +0200

    Fixing file structures

---
+++ frontend/src/Components/Styled/Theme.js
@@ -0,0 +1,19 @@
+export const theme = {
+	colors: {
+          Primær: "#D39D5B",
+          Sekundær: "#AD7A51",
+          Tertiær: "#30454C"
+	},
+	fonts: ['Titillium Web', 'Playfair Display'],
+	fontsizes: {
+		xs: '0.8rem',
+		s: '0.9rem',
+		m: '1rem',
+		l: '1.1rem',
+		xl: '1.5rem'
+	},
+	
+			}
+		
+	
+	
